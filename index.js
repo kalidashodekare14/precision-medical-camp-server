59d56cf31dd67165320a40e8f2e6f3b7c9d853d4
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-06-04 16:11:59 +0600

    Precision medical camp Assignment project Update

--- index.js
+++ index.js
@@ -50,18 +50,25 @@ async function run() {
         app.get('/users/organizer/:email', async (req, res) => {
             // DOTO: verify and email check
             const email = req.params.email
-            const query = {email: email}
+            const query = { email: email }
             const user = await usersCollcetion.findOne(query)
             console.log(user)
             let organizer = false
-            if(user){
+            if (user) {
                 organizer = user?.role === 'admin';
             }
-            res.send({organizer})
+            res.send({ organizer })
         })
 
         app.get('/popular-medical-camp', async (req, res) => {
             const result = await popularCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/popular-medical-camp/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await popularCollection.findOne(query)
             res.send(result)
         })
 
@@ -71,6 +78,8 @@ async function run() {
             const result = await popularCollection.findOne(query)
             res.send(result)
         })
+
+
 
         app.post('/popular-medical-camp', async (req, res) => {
             const item = req.body
@@ -98,6 +107,33 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/manage-update/:id', async (req, res) => {
+            const manage = req.body
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    camp_name: manage.camp_name,
+                    healthcare_professional: manage.managehealthcare_professional,
+                    date_and_time: manage.date_and_time,
+                    location: manage. location,
+                    camp_fees: manage.camp_fees,
+                    description: manage.description,
+                    image: manage.image
+                    
+                }
+            }
+            const result = await popularCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+        app.delete('/popular-medical-camp/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await popularCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
 
